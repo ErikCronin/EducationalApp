6fb0ea405656e1030095c338de2f4fcfa20f665b
ErikCronin <erik.cronin@my.jcu.edu.au>
2020-06-07 21:54:27 +1000

    implemented shake sensor

--- app/src/main/java/com/erik/educationalapp/MainActivity.java
+++ app/src/main/java/com/erik/educationalapp/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
@@ -22,7 +29,14 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 @SuppressLint("SetTextI18n")
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accSensor;
+    private boolean isAccAvail, isNotFirstTime;
+    private float lastX, lastY, lastZ;
+    private Vibrator vibrator;
+
     public static final String EXTRA_DIFFICULTY = "extraDifficulty";
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String KEY_HIGHSCORE = "keyHighscore";
@@ -118,8 +132,57 @@ public class MainActivity extends AppCompatActivity {
         soundConfirm = soundPool.load(this, R.raw.ok, 1);
         soundNewHighscore = soundPool.load(this, R.raw.newhighscore, 1);
 
-
         player.start();
+
+        /* Shake Sensor */
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        //Check if sensor is available
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            isAccAvail = true;
+        } else {
+            isAccAvail = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float currentX = sensorEvent.values[0];
+        float currentY = sensorEvent.values[1];
+        float currentZ = sensorEvent.values[2];
+
+        if (isNotFirstTime) {
+            float xDifference = Math.abs(lastX - currentX);
+            float yDifference = Math.abs(lastY - currentY);
+            float zDifference = Math.abs(lastZ - currentZ);
+
+            float shakeThreshold = 5f;
+            if ((xDifference > shakeThreshold && yDifference > shakeThreshold)
+                    || (xDifference > shakeThreshold && zDifference > shakeThreshold)
+                    || (yDifference > shakeThreshold && zDifference > shakeThreshold)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    //when phone is shaken it will do this
+                    vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                    Intent intent = new Intent(this, QuizActivity.class);
+                    startActivity(intent);
+                } else {
+                    vibrator.vibrate(500);
+                    //depreciated in API 26
+                }
+            }
+        }
+
+        lastX = currentX;
+        lastY = currentY;
+        lastZ = currentZ;
+        isNotFirstTime = true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     public void startQuiz(View view) {
@@ -177,6 +240,24 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putInt(KEY_HIGHSCORE, highscore);
         editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (isAccAvail) {
+            sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isAccAvail) {
+            sensorManager.unregisterListener(this);
+        }
     }
 
     //Releases SoundPool so it doesn't consume system resources
